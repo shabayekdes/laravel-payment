d86d3ea13dc1fe7c2698fd3076803a082febc71a
Esmail Shabayek <esmail.shabayek@gmail.com>
2021-11-30 00:24:57 +0200

    Add payment serverice provider class

---
+++ src/Providers/PaymentServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Shabayek\Payment\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class PaymentServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
