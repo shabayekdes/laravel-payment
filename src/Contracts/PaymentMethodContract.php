77d5a1cd67f8bd5da0096f79d2c5473fc5b879f9
Esmail Shabayek <esmail.shabayek@gmail.com>
2021-12-04 01:58:12 +0200

    Add payment method contract class

---
+++ src/Contracts/PaymentMethodContract.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Shabayek\Payment\Contracts;
+
+interface PaymentMethodContract
+{
+    /**
+     * Purchase with paymant mwthod and get redirect url
+     *
+     * @return string
+     */
+    public function purchase();
+
+    /**
+     * Pay with payment method.
+     *
+     * @return array
+     */
+    public function pay();
+}
