3a8ccdd55d91aca7bfecb60a473f14ff8b033fb2
Esmail Shabayek <esmail.shabayek@gmail.com>
2021-12-06 00:31:32 +0200

    Add cod method test caseclass

---
+++ tests/Unit/CodMethodTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Shabayek\Payment\Tests\Unit;
+
+use Shabayek\Payment\Facade\Payment;
+use Shabayek\Payment\Tests\TestCase;
+
+/**
+ * Class CodMethodTest
+ * @test
+ */
+class CodMethodTest extends TestCase
+{
+    /** @test*/
+    public function test_cod_method_should_have_required_function()
+    {
+        $method_id = 1;
+        $payment = Payment::store($method_id);
+
+        $this->assertTrue(method_exists($payment, 'purchase'));
+        $this->assertTrue(method_exists($payment, 'pay'));
+    }
+    /** @test*/
+    public function test_cod_method_purchase_should_return_null_value()
+    {
+        $method_id = 1;
+        $payment = Payment::store($method_id);
+
+        $this->assertNull($payment->purchase());
+    }
+    /** @test*/
+    public function test_cod_method_should_pay_function_return_array_of_data()
+    {
+        $method_id = 1;
+        $payment = Payment::store($method_id);
+        $pay = $payment->pay();
+
+        $this->assertCount(3, $pay);
+        $this->assertArrayHasKey('success', $pay);
+    }
+    /** @test*/
+    public function test_cod_method_should_pay_function_return_success()
+    {
+        $method_id = 1;
+        $payment = Payment::store($method_id);
+        $pay = $payment->pay();
+
+        $this->assertArrayHasKey('success', $pay);
+        $this->assertTrue($pay['success']);
+    }
+}
