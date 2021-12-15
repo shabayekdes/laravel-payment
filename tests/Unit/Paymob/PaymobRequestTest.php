74b251c1568fee50f7975cb2a9264e20df8a26ae
Esmail Shabayek <esmail.shabayek@gmail.com>
2021-12-16 00:36:19 +0200

    Add test if create order without customer details paymob

--- tests/Unit/Paymob/PaymobRequestTest.php
+++ tests/Unit/Paymob/PaymobRequestTest.php
@@ -30,19 +30,19 @@ class PaymobRequestTest extends TestCase
         $this->assertEquals(gettype($token), 'string');
     }
     /** @test*/
-    public function test_authication_token_in_paymob_method()
+    public function test_create_order_without_set_customer_details_in_paymob_method()
     {
+        $this->expectException(\Exception::class);
+
         Http::fake([
             // Stub a JSON response for paymob endpoints...
-            'https://accept.paymobsolutions.com/api/*' => Http::response(['token' => Str::random(512)], 200),
+            'https://accept.paymobsolutions.com/api/*' => Http::response([], 200),
         ]);
 
         $method_id = 2;
         $payment = Payment::store($method_id);
 
-        $token = $this->callMethod($payment, 'getAuthenticationToken');
-
-        $this->assertNotEquals(false, $token);
-        $this->assertEquals(gettype($token), 'string');
+        $token = Str::random(512);
+        $order = $this->callMethod($payment, 'orderCreation', [$token]);
     }
 }
