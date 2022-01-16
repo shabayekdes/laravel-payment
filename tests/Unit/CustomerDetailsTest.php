ab4a6d2b3ea27c7017a36236f25b58f74ccd699c
Esmail Shabayek <esmail.shabayek@gmail.com>
2022-01-17 00:49:42 +0200

    Add test case for customer details

---
+++ tests/Unit/CustomerDetailsTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Shabayek\Payment\Tests\Unit;
+
+use Mockery;
+use Mockery\MockInterface;
+use Illuminate\Http\Request;
+use Shabayek\Payment\Facade\Payment;
+use Shabayek\Payment\Tests\TestCase;
+use Shabayek\Payment\Tests\Fixtures\User;
+
+/**
+ * Class CustomerDetailsTest.
+ *
+ * @test
+ */
+class CustomerDetailsTest extends TestCase
+{
+    /** @test*/
+    public function test_can_get_customer_details_successfully()
+    {
+        // Mock user model
+        $mock = $this->partialMock(User::class, function (MockInterface $mock) {
+            $mock->shouldReceive('firstNameColumn')->once()->andReturn('John');
+        });
+        $mock->first_name = 'Changed';
+        $mock->last_name = 'Doe';
+        $mock->email = 'test@test.com';
+        $mock->phone = '01000000000';
+
+        $payment = Payment::store(2);
+        $payment->customer($mock);
+
+        $customerDetails = $this->callMethod($payment, 'getCustomerDetails');
+
+        $this->assertEquals('John', $customerDetails['first_name']);
+        $this->assertEquals('Doe', $customerDetails['last_name']);
+    }
+}
