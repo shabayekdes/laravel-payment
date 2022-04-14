b29301a759d22a440c6d83c0dffc69ae59985c34
Esmail Shabayek <esmail.shabayek@gmail.com>
2022-04-15 01:30:58 +0200

    Add visa paytabs to payment config

--- config/payment.php
+++ config/payment.php
@@ -45,12 +45,12 @@ return [
          */
         2 => [
             'provider'       => 'paymob',
-            'gateway'        => 'visa',
+            'gateway'        => 'visa-paymob',
             'is_active'      => true,
             'is_online'      => true,
             'is_installment' => false,
             'name'           => 'Credit / Debit card',
-            'logo'           => 'images/payment_methods/credit_payment.png',
+            'logo'           => 'images/credit_payment.png',
             'credentials'    => [
                 'api_key'        => env('PAYMOB_API_KEY'),
                 'hmac_hash'      => env('PAYMOB_HMAC_HASH'),
@@ -64,12 +64,12 @@ return [
          */
         3 => [
             'provider'       => 'mastercard',
-            'gateway'        => 'qnb',
+            'gateway'        => 'visa-qnb',
             'is_active'      => true,
             'is_online'      => true,
             'is_installment' => false,
             'name'           => 'Credit / Debit card QNB',
-            'logo'           => 'images/payment_methods/credit_payment.png',
+            'logo'           => 'images/credit_payment.png',
             'credentials'    => [
                 'username'     => env('QNB_USERNAME'),
                 'password'     => env('QNB_PASSWORD'),
@@ -77,6 +77,24 @@ return [
                 'callback_url' => env('QNB_CALLBACK_URL'),
                 'checkout_js'  => env('QNB_CHECKOUT_JS'),
                 'merchant_id'  => env('QNB_MERCHANT_ID'),
+            ],
+        ],
+        /**
+         * Credit / Debit card Paytabs.
+         */
+        4 => [
+            'provider'       => 'paytabs',
+            'gateway'        => 'visa-paytabs',
+            'is_active'      => true,
+            'is_online'      => true,
+            'is_installment' => false,
+            'name'           => 'Credit / Debit card Paytabs',
+            'logo'           => 'images/credit_payment.png',
+            'credentials'    => [
+                'base_url'     => 'https://secure-egypt.paytabs.com/',
+                'callback_url' => env('PAYTABS_CALLBACK_URL'),
+                'server_key'   => env('PAYTABS_SERVER_KEY'),
+                'profile_id'   => env('PAYTABS_PROFILE_ID')
             ],
         ],
     ],
