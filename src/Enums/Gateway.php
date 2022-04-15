dd1febdadb5cf51f30f3f51d2bdaf5feef83167e
Esmail Shabayek <esmail.shabayek@gmail.com>
2022-04-15 02:29:24 +0200

    Add verify paytabs method

--- src/Enums/Gateway.php
+++ src/Enums/Gateway.php
@@ -8,6 +8,9 @@ class Gateway
     public const GET_GO = 'get_go';
     public const SHAHRY = 'shahry';
     public const SOUHOOLA = 'souhoola';
+
     public const MASTERCARD_STATUS_APPROVED = 'APPROVED';
     public const MASTERCARD_RESPONSE_SUCCESS = 'SUCCESS';
+
+    public const PAYTABS_RESPONSE_SUCCESS = 'A';
 }
